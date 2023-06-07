322c7222f8ea65e7dd3f44791c645e638cbffd90
Aroko <122141457+Aroko001@users.noreply.github.com>
2023-06-07 19:17:50 +0900

    f

--- cogs/python.py
+++ cogs/python.py
@@ -11,17 +11,17 @@ class Modal(ui.Modal, title="コード実行"):
     async def on_submit(self, interaction: discord.Interaction):
         if "import os" in str(self.codes) or "import sys" in str(self.codes):
           return await interaction.response.send_message(embed=discord.Embed(title="エラー", description="コード内に`os`と`sys`はインポートできません。", color=discord.Color.red()), ephemeral=True)
-        if "open" in self.codes:
+        if "open" in str(self.codes):
           return await interaction.response.send_message(embed=discord.Embed(title="エラー", description="ファイルの操作はできません。", color=discord.Color.red()),ephemeral=True)
-        if self.codes == "juusou":
+        if str(self.codes) == "juusou":
           return await interaction.response.send_message("https://media.discordapp.net/attachments/1015482770845335652/1104262558338273411/DDLVID.COM-1648698069645680641_1.gif", ephemeral=True)
-        if self.codes == "bocchitherock":
+        if str(self.codes) == "bocchitherock":
           return await interaction.response.send_message("https://tenor.com/view/bocchi-bocchitherock-hitori-gotou-%E3%81%BC%E3%81%A3%E3%81%A1%E3%81%96%E3%82%8D%E3%81%A3%E3%81%8F-anime-gif-26895035", ephemeral=True)
         with open("././temp.py", "w") as f:
-          f.write(self.codes)
+          f.write(str(self.codes))
         result = subprocess.run(["python", "temp.py"], capture_output=True, text=True)
         embed = discord.Embed(title="Python", color=discord.Color.green())
-        embed.add_field(name="コード", value=f"```python\n{self.codes}```", inline=False)
+        embed.add_field(name="コード", value=f"```python\n{str(self.codes)}```", inline=False)
         embed.add_field(name="出力", value=f"```{result.stdout}\n{result.stderr}```")
         channel = self.bot.get_channel(1098527872811012146)
         embed2 = discord.Embed(title=f"Python - {interaction.user}|{interaction.user.id}", description=f"code: ```{self.codes}```\nresult:```{result.stdout}\n{result.stderr}```", color=discord.Color.green())
