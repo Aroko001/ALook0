d44808ef6d63ffaad37c8b7dbf5435d6dab1aac6
Aroko <122141457+Aroko001@users.noreply.github.com>
2023-06-11 07:07:07 +0900

    統合

--- cogs/help.py
+++ cogs/help.py
@@ -38,7 +38,7 @@ class HelpSelect(discord.ui.Select):
             ephemeral=True
         )
 
-class help(commands.Cog):
+class 情報(commands.Cog):
     def __init__(self, bot: commands.Bot) -> None:
         self.bot: commands.Bot = bot
 
@@ -46,10 +46,31 @@ class help(commands.Cog):
     async def help(self, i: discord.Interaction):
         embed = discord.Embed(
             title="ヘルプ",
-            description=f"多機能なBot, **ALook0**\n\nお問い合わせや要望は[Ar0ko192](https://discord.com/users/790489873957781536)へお願いします。\n\n起動時刻: <t:{self.bot.kidoutime}:F>\n\n招待はこちらから↓\n[管理者権限](https://discord.com/oauth2/authorize?client_id=1107181590695645294&scope=bot+applications.commands&permissions=8)\n[権限なし](https://discord.com/oauth2/authorize?client_id=1107181590695645294&scope=bot+applications.commands)"
+            description=f"多機能なBot, **ALook0**\n\nお問い合わせや要望は/feature_suggestionコマンドでお願いします。\n\n起動時刻: <t:{self.bot.kidoutime}:F>\n\n招待はこちらから↓\n[管理者権限](https://discord.com/oauth2/authorize?client_id=1107181590695645294&scope=bot+applications.commands&permissions=8)\n[権限なし](https://discord.com/oauth2/authorize?client_id=1107181590695645294&scope=bot+applications.commands)"
         )
         view = discord.ui.View().add_item(HelpSelect(self.bot))
         await i.response.send_message(embed=embed, view=view)
 
+    @app_commands.command(name="ping", description="BotのPing値を表示します。")
+    async def ping(self, i: discord.Interaction):
+        embed = discord.Embed(title="Pong! :ping_pong:", description=f"現在のBotのPing値は{round(self.bot.latency * 1000)}msです。", color=discord.Color.green())
+        await i.response.send_message(embed=embed)
+
+    @app_commands.command(name="bug_report", description="バグ報告をします。")
+    @app_commands.describe(description="報告する内容を入力してください。")
+    async def bugreport(self, i: discord.Interaction, description: str):
+        channel = self.bot.get_channel(1095604986051842138)
+        embed = discord.Embed(title=f"バグ報告 - {i.user}|{i.user.id} - {i.guild}", description=description, color=discord.Color.green())
+        await channel.send(embed=embed)
+        await i.response.send_message("送信しました。ご協力ありがとうございます。", ephemeral=True)
+
+    @app_commands.command(name="feature_suggestion", description="機能提案をします。")
+    @app_commands.describe(description="提案する内容を入力してください。")
+    async def featuresuggestion(self, i: discord.Interaction, description: str):
+        channel = self.bot.get_channel(1095609040589037648)
+        embed = discord.Embed(title=f"機能提案 - {i.user}|{i.user.id} - {i.guild}", description=description, color=discord.Color.green())
+        await channel.send(embed=embed)
+        await i.response.send_message("送信しました。ご協力ありがとうございます。", ephemeral=True)
+
 async def setup(bot: commands.Bot) -> None:
-    await bot.add_cog(help(bot))
+    await bot.add_cog(情報(bot))
